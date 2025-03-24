c031776e6204c42780df4ba144dd2f67558b8b9c
Matty Evans <mattevansnz@gmail.com>
2025-03-25 07:46:30 +1000

    feat: add documentation for PandaPulse and tooling section
    This commit introduces documentation for the PandaPulse tool,
    providing usage instructions, command details, and permission
    information. Additionally, it sets up a new "Tooling" section
    in the documentation site with an introductory page.
    
    The changes include:
    
    - Creation of `docs/tooling/PandaPulse/intro.md` with detailed
      information on how to use PandaPulse, including command
      descriptions, and permission details.
    - Creation of `docs/tooling/intro.md` as an introductory page
      for the "Tooling" section.
    - Modification of `docusaurus.config.js` to include a "Tooling"
      link in the navigation bar.
    - Modification of `sidebars.js` to include a `toolingSidebar`
      for the new "Tooling" section.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,6 +76,12 @@ const config = {
             label: 'Xatu',
           },
           {
+            type: 'doc',
+            docId: 'tooling/intro',
+            position: 'left',
+            label: 'Tooling',
+          },
+          {
             href: 'https://github.com/ethpandaops/public-docs',
             label: 'GitHub',
             position: 'right',
